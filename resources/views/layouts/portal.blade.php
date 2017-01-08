a7056d0805439d3d060ee88311357fc4b620cb54
CODEheures <webmaster@codeheures.fr>
2017-01-08 21:25:42 +0100

    ajoute favicons

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -13,6 +13,15 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ elixir("css/app.css", '') }}">
     @yield('css')
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#dca300">
+    <meta name="apple-mobile-web-app-title" content="DestockEurope">
+    <meta name="application-name" content="DestockEurope">
+    <meta name="theme-color" content="#dca300">
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode([
