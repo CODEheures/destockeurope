c823f6367bea2c53de7a54f4af792fc2be66de1d
CODEheures <webmaster@codeheures.fr>
2017-04-28 00:15:35 +0200

    add NOTIFICATION_ACTIVE test on kernel schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -95,40 +95,41 @@ class Kernel extends ConsoleKernel
 
         //Scheduler for Notifications
         $schedule->call(function(){
-            $status = '0';
-            $msgTxt='';
-            try {
-                $stat = Stats::whereDate('created_at', Carbon::yesterday())->first();
-                if($stat && ($stat->totalNewFreeAdverts+$stat->totalNewCostAdverts)>0){
-                    $msgTxt = ($stat->totalNewFreeAdverts+$stat->totalNewCostAdverts) . ' nouvelles annonces aujourd\'hui';
+            if(env('NOTIFICATION_ACTIVE')==true){
+                $status = '0';
+                $msgTxt='';
+                try {
+                    $stat = Stats::whereDate('created_at', Carbon::yesterday())->first();
+                    if($stat && ($stat->totalNewFreeAdverts+$stat->totalNewCostAdverts)>0){
+                        $msgTxt = ($stat->totalNewFreeAdverts+$stat->totalNewCostAdverts) . ' nouvelles annonces aujourd\'hui';
 
-                    $server_key = env('GOOGLE_FIREBASE_SERVERAPIKEY');
-                    $client = new Client();
-                    $client->setApiKey($server_key);
-                    $client->injectGuzzleHttpClient(new \GuzzleHttp\Client());
+                        $server_key = env('GOOGLE_FIREBASE_SERVERAPIKEY');
+                        $client = new Client();
+                        $client->setApiKey($server_key);
+                        $client->injectGuzzleHttpClient(new \GuzzleHttp\Client());
 
-                    $message = new Message();
-                    $message->setPriority('high');
-                    $message->setTimeToLive(60*60*24);
-                    $message->addRecipient(new Topic(config('topics.1')));
-                    $notification = new Notification(config('app.name'), $msgTxt);
-                    $notification->setClickAction(route('portal'));
-                    $notification->setIcon('/android-chrome-512x512.png');
-                    $message
-                        ->setNotification($notification)
-                    ;
-                    $response = $client->send($message);
-                    $status = $response->getStatusCode();
+                        $message = new Message();
+                        $message->setPriority('high');
+                        $message->setTimeToLive(60*60*24);
+                        $message->addRecipient(new Topic(config('topics.1')));
+                        $notification = new Notification(config('app.name'), $msgTxt);
+                        $notification->setClickAction(route('portal'));
+                        $notification->setIcon('/android-chrome-512x512.png');
+                        $message
+                            ->setNotification($notification)
+                        ;
+                        $response = $client->send($message);
+                        $status = $response->getStatusCode();
+                    }
+                } catch (\Exception $e) {
+                    $status = '500';
+                    $msgTxt = $e->getMessage();
                 }
-            } catch (\Exception $e) {
-                $status = '500';
-                $msgTxt = $e->getMessage();
+                if(!Storage::disk('logs')->exists('notifications.log')){
+                    Storage::disk('logs')->append('notifications.log' , 'DATE;STATUS_CODE;MESSAGE');
+                }
+                Storage::disk('logs')->append('notifications.log' , Carbon::now()->toDateTimeString() . ';' . $status . ';' . $msgTxt );
             }
-            if(!Storage::disk('logs')->exists('notifications.log')){
-                Storage::disk('logs')->append('notifications.log' , 'DATE;STATUS_CODE;MESSAGE');
-            }
-            Storage::disk('logs')->append('notifications.log' , Carbon::now()->toDateTimeString() . ';' . $status . ';' . $msgTxt );
-
         })->dailyAt('6:48');
 
         //Scheduler for Update GEOIP
