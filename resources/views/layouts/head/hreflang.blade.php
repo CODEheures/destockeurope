529d195a02eca6c467ce7ba2d4662ec6fea1c096
CODEheures <webmaster@codeheures.fr>
2017-06-17 16:43:16 +0200

    add hreflang x-default

--- resources/views/layouts/head/hreflang.blade.php
+++ resources/views/layouts/head/hreflang.blade.php
@@ -1,4 +1,5 @@
 @if(!auth()->check() && \App\Common\RouteUtils::routeHaveLangParam())
+    <link rel="alternate" hreflang="x-default" href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(), \Illuminate\Support\Facades\Route::current()->parameters) }}" />
     @foreach(config('codeheuresUtils.availableLocales') as $lang)
         <link rel="alternate" hreflang="{{ $lang }}" href="{{ route(\Illuminate\Support\Facades\Route::currentRouteName(), array_merge(\Illuminate\Support\Facades\Route::current()->parameters, ['lang'=>$lang])) }}"/>
     @endforeach
