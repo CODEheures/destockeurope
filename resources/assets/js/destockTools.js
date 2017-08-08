eb72b972a0c113a664aaf0353bfa6bcf2816fff0
CODEheures <webmaster@codeheures.fr>
2017-08-08 17:01:47 +0200

    add truncFloat to margins

--- resources/assets/js/destockTools.js
+++ resources/assets/js/destockTools.js
@@ -39,26 +39,36 @@ let destockTools = {
             if(forSeller){
                 unitMargin =  advert.originalPrice-advert.buyingPrice;
                 unitMargin = Math.floor(unitMargin)/Math.pow(10,advert.priceSubUnit);
+                unitMargin = this.truncFloat(unitMargin, advert.priceSubUnit);
 
                 lotMiniMargin = unitMargin*advert.lotMiniQuantity;
+                lotMiniMargin = this.truncFloat(lotMiniMargin, advert.priceSubUnit);
 
                 totalMargin = originalTotalSellerPrice-advert.buyingPrice*advert.totalQuantity;
                 totalMargin = Math.floor(totalMargin)/Math.pow(10,advert.priceSubUnit);
+                totalMargin = this.truncFloat(totalMargin, advert.priceSubUnit);
 
                 globalDiscount = advert.discount_on_total;
             } else {
                 unitMargin =  advert.originalPrice*advert.price_coefficient/100;
                 unitMargin = Math.floor(unitMargin)/Math.pow(10,advert.priceSubUnit);
+                unitMargin = this.truncFloat(unitMargin, advert.priceSubUnit);
 
                 lotMiniMargin = unitMargin*advert.lotMiniQuantity;
+                lotMiniMargin = this.truncFloat(lotMiniMargin, advert.priceSubUnit);
 
                 totalMargin = originalTotalSellerPrice*advert.price_coefficient_total/100;
                 totalMargin = Math.floor(totalMargin)/Math.pow(10,advert.priceSubUnit);
+                totalMargin = this.truncFloat(totalMargin, advert.priceSubUnit);
 
                 priceMargin = unitSellerPrice + unitMargin;
+                priceMargin = this.truncFloat(priceMargin, advert.priceSubUnit);
+
                 totalPriceMargin = totalSellerPrice + totalMargin;
+                totalPriceMargin = this.truncFloat(totalPriceMargin, advert.priceSubUnit);
 
                 totalPriceByLotMargin = priceMargin*advert.totalQuantity;
+                totalPriceByLotMargin = this.truncFloat(totalPriceByLotMargin, advert.priceSubUnit);
 
                 globalDiscount = (1 - totalPriceMargin/totalPriceByLotMargin)*100;
             }
@@ -84,6 +94,10 @@ let destockTools = {
 
 
         return margins;
+    },
+
+    truncFloat: function (value, decimal) {
+        return parseFloat(value.toFixed(decimal));
     }
 };
 
